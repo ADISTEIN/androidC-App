d6df1f3090ec641b6e37c91639b3a143101c2072
ADISTEIN <adistein102@gmail.com>
2023-05-08 19:01:08 +0530

    Code done 1st assignment

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button,  Alert } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [text, onChangeText] = useState('');
+  const [number, onChangeNumber] = useState('');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView >
+      <View style={styles.comp}>
+      <Text style={styles.text}>Username</Text>
+      <TextInput
+      placeholder='Enter Username'
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Text style={styles.text}>Password</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+<Button
+        title="Log In"
+        onPress={() => Alert.alert('Logged In')}
+      />
+      </View>
+    </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
+  text: {
+    margin:12
+  },
+  comp: {
+  paddingTop: 200
+  }
+
 });
